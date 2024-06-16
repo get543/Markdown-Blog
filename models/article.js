e77b8e755b8bb640d5db9342efead6baee618043
Glitch (blossom-fire-spandex) <none>
2024-06-16 12:49:36 +0700

    update marked code

--- models/article.js
+++ models/article.js
@@ -1,29 +1,10 @@
 const mongoose = require("mongoose"); // db
-// const { Marked } = require("marked"); // md to html converter
-// const { markedHighlight } = require("marked-highlight"); // code highlight
-const marked = require("marked");
-// const pygmentize = require("pygmentize-bundled"); // code highlight
+const { Marked } = require("marked"); // md to html converter
 const slugify = require("slugify"); // repace id in url with something prettier
 const createDomPurify = require("dompurify"); // sanitised converted html
 const { JSDOM } = require("jsdom"); // additional package to sanitised html
 
-// const marked = new Marked(
-//   markedHighlight({
-//     async: true,
-//     highlight(code, lang) {
-//       return new Promise((resolve) => {
-//         pygmentize({ lang, format: "html" }, code, (err, result) => {
-//           if (err) {
-//             resolve(err);
-//             return;
-//           }
-
-//           resolve(result.toString());
-//         });
-//       });
-//     },
-//   })
-// );
+const marked = new Marked();
 
 const dompurify = createDomPurify(new JSDOM().window);
 
@@ -60,7 +41,7 @@ articleSchema.pre("validate", function (next) {
   }
 
   if (this.markdown) {
-    this.sanitizedHtml = dompurify.sanitize(marked(this.markdown));
+    this.sanitizedHtml = dompurify.sanitize(marked.parse(this.markdown));
   }
 
   next();
